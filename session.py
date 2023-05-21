cf83d6f87601e6f8ae864a0223b7ed7b265d4253
Yonas <yogiorno05@gmail.com>
2023-05-21 01:28:31 -0400

    Added lots, working on multilingualizing BWs

--- session.py
+++ session.py
@@ -3,9 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
 import time
 
-from bw import classicBW
+#from bw import classicBW
 
 class session:
 
@@ -21,26 +22,60 @@ class session:
         self.logIn()
 
     # Wait for function
-    def waitFor(self, , by, element):
+    def waitFor(self, by, element):
         try:
             wait = WebDriverWait(self.driver, 20).until(
-                if by == "id":
-                    EC.presence_of_element_located((By.ID, ))
+                EC.presence_of_element_located((eval("By." + by), element))
             )
+        except:
+            print("There seems to have been an error.")
+            print("Looking for", by, "of", element)
+            confirm = input("Press enter to continue\n")
+            if confirm != "": self.driver.close()
+    
+    # Reloading the page
+    def reload(self):
+        self.driver.find_element(By.TAG_NAME, "body").send_keys(Keys.COMMAND + "r")
+
+    # Going to a page
+    def goTo(self, page):
+        location = self.home.replace("llworkspace", page)
+        self.driver.get(location)
     
     # Logging in
     def logIn(self):
         self.driver.find_element(By.ID, "otds_username").send_keys("Admin")
         self.driver.find_element(By.ID, "otds_password").send_keys("livelink" + Keys.RETURN)
-        time.sleep(10000)
+        print("Logged in")
 
     # Changing the language
-    def changeLang(lang):
+    def changeLang (self, lang):
+        self.goTo("personal.settings")
+        self.waitFor("ID", "metadataLang")
+        Select(self.driver.find_element(By.ID, "metadataLang")).select_by_value(lang.lower())
+        self.driver.find_element(By.CLASS_NAME, "saveButton").click()
+        self.reload()
+        self.driver.back()
+        self.driver.back()
+        print("Changed language to", lang)
 
+    # Multilingualizing the Workspace
+    def multilingualizeWorkspace(self):
+        self.goTo("ll&objType=133&objAction=browse")
+        #BWWSID = self.getIDFromText("BW WS")
+        #self.driver.find_element(By.ID, self.getIDFromText("BW WS")).click()
+        #self.driver.find_element(By.XPATH, "//a[@title='Functions']").click()
+        self.driver.find_element(By.XPATH, "//a[@title='Function menu for BW WS']").click()
+        self.driver.find_element(By.XPATH, "//*[contains(@id, 'menuItem_Properties')]").click()
+        time.sleep(20)
 
-    # Multilingualising the workspace attributes
-    #def multilingualiseAttributes(self):
+    # Getting the id from text
+    def getIDFromText(self, text):
+        print(text)
+        id = self.driver.find_element(By.LINK_TEXT, text)
+        return id.get_attribute("id")
 
+    # Creating a Workspace
     def createBW(self, num, SDL, nameSDL, nameUDL, attSDL, attUDL, VMnum):
         temp = classicBW(num, SDL, nameSDL, nameUDL, attSDL, attUDL, VMnum)
         self.bws.append(temp)
--- startup.py
+++ startup.py
@@ -3,3 +3,4 @@ from session import session
 VMnum = input("Which VM are you running?\n")
 thisSession = session(VMnum)
 
+thisSession.multilingualizeWorkspace()
