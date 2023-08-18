b34fdd805b7ddb4e7882165ddbac6e1d481a6788
Yonas <yogiorno05@gmail.com>
2023-08-18 18:21:18 -0400

    SV Folder Creation Passed

--- Smart_View.py
+++ Smart_View.py
@@ -7,10 +7,16 @@ thisSession = svTest(VMnum)
 f = False; t = True
 
 # Create Folder -> Num, inSDL, infoSDL, infoUDL, Pause
-thisSession.createFolder(1, t, t, t, t)
+thisSession.createFolder(1, t, t, t, f)
 thisSession.createFolder(2, t, t, f, f)
 thisSession.createFolder(3, t, f, t, f)
 thisSession.createFolder(4, f, t, t, f)
 thisSession.createFolder(5, f, f, t, f)
 thisSession.createFolder(6, f, t, f, f)
 
+# Other Smart View Tests
+#thisSession.checkFilters(t)
+#thisSession.checkFilters(f)
+#thisSession.checkFolder()
+
+del thisSession
